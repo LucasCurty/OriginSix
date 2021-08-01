7d9814d7cdd2ad596ef81c8790d90f9eb34fbb7c
Lucas Curty <74004642+LucasCurty@users.noreply.github.com>
2021-08-01 12:15:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+/* opens and closes the menu when clicking on the icon*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* When clicking on the icon, hidden the menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/* Testimonials carousel slider  swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/*ScrollReveal: Mostrar elementos quando der scroll na página*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `
+#home .text,#home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links,
+footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+/* change the header of page with user scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderUserScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+/* Button back to top */
+
+function backToTopButton() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Active menu according to the visible section on the page*/
+const sections = document.querySelectorAll('main section[id]')
+function activeMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+/* When scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderUserScroll()
+  backToTopButton()
+  activeMenuAtCurrentSection()
+})
